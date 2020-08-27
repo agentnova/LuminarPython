8f73eec37d6e4f4e2107c1372c708e1e02d61a57
agentnova <arjunaj2234@gmail.com>
2020-08-27 15:57:39 +0530

    tuple

--- Luminaarpython/collections/Tuple/tupledemo.py
+++ Luminaarpython/collections/Tuple/tupledemo.py
@@ -5,3 +5,8 @@ print(a)
 # insertion order presered
 # immutable
 # can store multiple types of values
+
+from functools import *
+y=list(filter(lambda x:x<4,[1,2,3,4,5]))
+print(y)
+print(tuple(y))
